4680992ea743933e110b515202ace21c5238bc26
Dora Urbalejo <dora_ur@hotmail.com>
2012-01-15 21:20:25 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrebolApp::Application.config.secret_token = 'a18f534c84bea939dd960426ef0932da14a5f60685cc4c2d49db578f2b06bd4a468101a5dd446c37ad76b02bb0b55c844a683367d18b07a76ee06d20f621495e'
