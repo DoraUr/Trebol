52e301b95628c5497edd7afe4c07f09487e0f46c
Dora Urbalejo <dora_ur@hotmail.com>
2012-01-16 00:29:19 -0800

    Added a site layout

--- Gemfile
+++ Gemfile
@@ -3,12 +3,16 @@ source 'http://rubygems.org'
 gem 'rails', '3.0.1'
 
 gem 'sqlite3-ruby','1.3.2', :require => 'sqlite3'
+gem 'gravatar_image_tag', '1.0.0.pre2'
 
 group :development do
   gem 'rspec-rails', '2.6.1'
+  gem 'annotate', '2.4.0'
 end
 
 group :test do
   gem 'rspec-rails', '2.6.1'
   gem 'webrat', '0.7.1'
+  gem 'growl'
+  gem 'factory_girl_rails', '1.0'
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,18 +1,51 @@
 require 'spec_helper'
 
 describe PagesController do
+  
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                :content =>"Grupo Trebol - Servicios Administrativos | Home")
+    end
+    
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
+    
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                :content =>"Grupo Trebol - Servicios Administrativos | Contact")
+    end
+  end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                :content =>"Grupo Trebol - Servicios Administrativos | About")
     end
   end
 
