b08434167b7501840571515d73e674face21fc12
Dora Urbalejo <dora_ur@hotmail.com>
2012-02-02 10:36:55 -0800

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,15 @@ class UsersController < ApplicationController
     @user  = User.new
     @title = "Sign up"
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user, :flash => { :success => "Bienvenido a Grupo TRebol" }
+    else
+      @title = "Procesar"
+      render 'new'
+    end
+  end
 
 end
